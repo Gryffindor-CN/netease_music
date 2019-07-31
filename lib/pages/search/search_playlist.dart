29ca201af7ea3f5238a2ffa89e43c12468896f7d
lijianfeng <lijianfeng@lonntec.com>
2019-07-31 15:27:09 +0800

    修改搜索页面歌单区域样式

--- lib/pages/search/search_playlist.dart
+++ lib/pages/search/search_playlist.dart
@@ -54,115 +54,6 @@ class SearchPlaylistTabState extends State<SearchPlaylistTab>
     }
   }
 
-  Widget _buildWidget(PlayList item) {
-    String _keyword = widget.keyword;
-    Widget _nameWidget;
-
-    if (item.name.contains(_keyword)) {
-      var startIndex = item.name.indexOf(_keyword);
-      var itemNameLen = item.name.length;
-      var keywordLen = _keyword.length;
-      _nameWidget = DefaultTextStyle(
-        style: TextStyle(
-          fontSize: 15.0,
-          color: Colors.black,
-        ),
-        child: RichText(
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-          text: TextSpan(
-            text: item.name.substring(0, startIndex),
-            style: TextStyle(color: Colors.black),
-            children: <TextSpan>[
-              TextSpan(
-                  text: _keyword, style: TextStyle(color: Color(0xff0c73c2))),
-              TextSpan(
-                  text:
-                      item.name.substring(startIndex + keywordLen, itemNameLen),
-                  style: TextStyle(color: Colors.black)),
-            ],
-          ),
-        ),
-      );
-    } else {
-      _nameWidget = DefaultTextStyle(
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(fontSize: 15.0, color: Colors.black),
-        child: Text(
-          item.name,
-        ),
-      );
-    }
-
-    return InkWell(
-      onTap: () {},
-      child: Row(
-        children: <Widget>[
-          SizedBox(
-            width: 15.0,
-          ),
-          Expanded(
-              child: Container(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 45.0,
-                  height: 45.0,
-                  margin: EdgeInsets.only(right: 10.0),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                      child: Image.network(
-                        item.coverImgUrl,
-                      )),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        _nameWidget,
-                        SizedBox(
-                          height: 4.0,
-                        ),
-                        DefaultTextStyle(
-                          style: TextStyle(
-                              fontSize: 10.0, color: Color(0xFFBDBDBD)),
-                          child: Row(
-                            children: <Widget>[
-                              Text(
-                                '${item.trackCount}首音乐',
-                              ),
-                              SizedBox(
-                                width: 6.0,
-                              ),
-                              Text('by ${item.creatorName}'),
-                              SizedBox(
-                                width: 6.0,
-                              ),
-                              Text(
-                                  '播放${((item.playCount / 10000) * (pow(10, 1))).round() / (pow(10, 1))}万次'),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )),
-          SizedBox(
-            width: 15.0,
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return _isLoading == true
@@ -185,12 +76,6 @@ class SearchPlaylistTabState extends State<SearchPlaylistTab>
                         color: Theme.of(context).textTheme.subtitle.color),
                   ),
                 ))
-            // : ListView.builder(
-            //     itemCount: playlist.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       return _buildWidget(playlist[index]);
-            //     },
-            //   );
             : SingleChildScrollView(
                 child: Container(
                   child: PlaylistSection(widget.keyword, playlist),
