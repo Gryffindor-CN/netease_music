d2157c89a508c920969350ea531442d933eb28c2
lijianfeng <lijianfeng@lonntec.com>
2019-09-16 14:37:49 +0800

    1.解决ios不能播放播放远程音乐资源 2.修改字体大小

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 import 'package:netease_music/components/musicplayer/inherited_demo.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'dart:ui';
