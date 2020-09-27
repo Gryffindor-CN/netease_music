4faebaadf624560d175fca605b18f76fef335dc5
lijianfeng <lijianfeng@lonntec.com>
2020-09-27 09:55:38 +0800

    ~

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Volumes/e/Programfiles/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/e/workspace/flutter/netease_music"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Volumes/e/Programfiles/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
